fe377e77e3d316f96701c471e4527a8d2abe17ba
Duckforger <demonicboi69@gmail.com>
2024-11-10 03:09:31 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// service-worker.js
+
+const CACHE_NAME = "duck-clicker-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-512x512.png"
+];
+
+// Install the service worker and cache essential assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate the service worker and clean up old caches
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch request from cache or network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
